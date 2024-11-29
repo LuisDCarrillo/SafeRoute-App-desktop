2f7b1fe207aacd2b67cc97f3be54aa9d4f9a36f5
Daniel <1001.27539960.ucla@gmail.com>
2024-11-29 17:33:48 -0400

    primer commit

---
+++ src/main/java/com/mycompany/transporte/Transporte.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.transporte;
+import views.Inicio;
+/**
+ *
+ * @author SaraVega
+ */
+public class Transporte {
+
+    public static void main(String[] args) {
+         Inicio ini = new Inicio();
+        ini.setVisible(true);
+    }
+}
